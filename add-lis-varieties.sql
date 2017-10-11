c2c261908c775dd1f38048fdd9dc84faf89cbb42
Sam Hokin <shokin@ncgr.org>
2017-10-11 15:20:29 -0600

    Run-once scripts to add variety to species in chado.

---
+++ add-lis-varieties.sql
@@ -0,0 +1,21 @@
+--
+-- Add variety suffixes to chado organisms
+--
+
+UPDATE organism SET species='arietinum_CDCFrontier' WHERE organism_id=          15;
+UPDATE organism SET species='arietinum_ICC4958'     WHERE organism_id=          33;
+UPDATE organism SET species='max_Williams82'        WHERE organism_id=          16;
+UPDATE organism SET species='truncatula_A17'        WHERE organism_id=          21;
+UPDATE organism SET species='vulgaris_G19833'       WHERE organism_id=          24;
+UPDATE organism SET species='thaliana_Col0'         WHERE organism_id=           6;
+UPDATE organism SET species='duranensis_V14167'     WHERE organism_id=        1571;
+UPDATE organism SET species='ipaensis_K30076'       WHERE organism_id=        1572;
+UPDATE organism SET species='americana_AA-2155'     WHERE organism_id=          31;
+UPDATE organism SET species='cajan_ICPL87119'       WHERE organism_id=          14;
+UPDATE organism SET species='angularis_Gyeongwon'   WHERE organism_id=          32;
+UPDATE organism SET species='albus_LAGI01'          WHERE organism_id=          29;
+UPDATE organism SET species='angustifolius_Tanjil'  WHERE organism_id=          19;
+UPDATE organism SET species='pratense_MilvusB'      WHERE organism_id=          30;
+UPDATE organism SET species='radiata_VC1973A'       WHERE organism_id=          26;
+UPDATE organism SET species='japonicus_MG20'        WHERE organism_id=          18;
+UPDATE organism SET species='unguiculata_IT97K-499-35' WHERE organism_id=	27;
---
+++ add-peanutbase-varieties.sql
@@ -0,0 +1,5 @@
+-- add varieties to PeanutBase organisms
+
+UPDATE organism SET species='hypogaea_Various'  WHERE organism_id=13;
+UPDATE organism SET species='duranensis_V14167' WHERE organism_id=14;
+UPDATE organism SET species='ipaensis_K30076'   WHERE organism_id=15;
