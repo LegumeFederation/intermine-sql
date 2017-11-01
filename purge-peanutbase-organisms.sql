e87f5b6924b0e02e2b115482c0a8c7b35304dc40
Sam Hokin <shokin@ncgr.org>
2017-11-01 11:11:58 -0600

    Fixed comment.

--- purge-peanutbase-organisms.sql
+++ purge-peanutbase-organisms.sql
@@ -1,5 +1,6 @@
-##
-## get rid of all the extra peanut organisms that we don't have associated with taxon IDs and don't import
-##
+--
+-- Get rid of all the extra peanut organisms that we don't have associated with taxon IDs and don't import
+-- NOTE: this will take a while due to cascaded deletes.
+--
 
 DELETE FROM organism WHERE species NOT like '%\_%';
