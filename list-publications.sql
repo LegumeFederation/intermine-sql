83b2023936d34348fd154c00db5a06de15ec0679
Sam Hokin <shokin@ncgr.org>
2017-12-15 12:36:42 -0700

    Just another handy script.

--- list-publications.sql
+++ list-publications.sql
@@ -1,4 +1,4 @@
 --
 -- list out the publications in search of dupes
 --
-SELECT substring(title,1,100) AS title, count(*) FROM publication GROUP BY title ORDER BY count;
+SELECT substring(title,1,100) AS title, substring(journal,1,20) AS journal, count(*) FROM publication GROUP BY title,journal ORDER BY count,title;
