2756d480cde025ec8fa85eb6c054d72772ba9e2e
Sam Hokin <shokin@ncgr.org>
2018-03-09 10:53:27 -0700

    Tweaks.

--- list-ontology-terms.sql
+++ list-ontology-terms.sql
@@ -1,10 +1,12 @@
 --
 -- list out the ontology terms, to find dupes
 --
-SELECT ontologyid,id,identifier,substring(name,1,20) AS name,substring(description,1,20) AS description FROM ontologyterm 
+SELECT 'NULL' AS ontologyid,id,identifier,substring(name,1,20) AS name,substring(description,1,20) AS description FROM ontologyterm
 	WHERE ontologyid IS NULL ORDER BY identifier,name LIMIT 10;
 SELECT ontologyid,id,identifier,substring(name,1,20) AS name,substring(description,1,20) AS description FROM ontologyterm 
         WHERE ontologyid=1000000 ORDER BY identifier,name LIMIT 10;
+SELECT ontologyid,id,identifier,substring(name,1,20) AS name,substring(description,1,20) AS description FROM ontologyterm
+        WHERE ontologyid=3000000 ORDER BY identifier,name LIMIT 10;
 SELECT ontologyid,id,identifier,substring(name,1,20) AS name,substring(description,1,20) AS description FROM ontologyterm 
         WHERE ontologyid=2000000 ORDER BY identifier,name LIMIT 10;
 SELECT ontologyid,id,identifier,substring(name,1,20) AS name,substring(description,1,20) AS description FROM ontologyterm 
