027c0456207ede67411e75352fa713fb7dd7701b
Sam Hokin <shokin@ncgr.org>
2018-03-12 10:46:43 -0600

    Don't rely on IDs, use term prefixes instead.

--- list-ontology-terms.sql
+++ list-ontology-terms.sql
@@ -2,12 +2,12 @@
 -- list out the ontology terms, to find dupes
 --
 SELECT ontologyid,id,identifier,substring(name,1,20) AS name,substring(description,1,20) AS description FROM ontologyterm 
-	WHERE ontologyid IS NULL ORDER BY identifier,name LIMIT 10;
+	WHERE identifier LIKE 'SO:%' ORDER BY identifier,name LIMIT 5;
 SELECT ontologyid,id,identifier,substring(name,1,20) AS name,substring(description,1,20) AS description FROM ontologyterm 
-        WHERE ontologyid=1000000 ORDER BY identifier,name LIMIT 10;
+        WHERE identifier LIKE 'GO:%' ORDER BY identifier,name LIMIT 5;
 SELECT ontologyid,id,identifier,substring(name,1,20) AS name,substring(description,1,20) AS description FROM ontologyterm 
-        WHERE ontologyid=2000000 ORDER BY identifier,name LIMIT 10;
+        WHERE identifier LIKE 'PO:%' ORDER BY identifier,name LIMIT 5;
 SELECT ontologyid,id,identifier,substring(name,1,20) AS name,substring(description,1,20) AS description FROM ontologyterm 
-        WHERE ontologyid=4000000 ORDER BY identifier,name LIMIT 10;
+	WHERE identifier LIKE 'TO:%' ORDER BY identifier,name LIMIT 5;
 SELECT ontologyid,id,identifier,substring(name,1,20) AS name,substring(description,1,20) AS description FROM ontologyterm 
-        WHERE ontologyid=5000000 ORDER BY identifier,name LIMIT 10;
+        WHERE identifier LIKE 'CO%'  ORDER BY identifier,name LIMIT 5;
