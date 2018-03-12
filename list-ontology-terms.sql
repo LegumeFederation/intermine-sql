59d9c6e5707695ccd74be42d2a979e2d98ac9b04
Sam Hokin <shokin@ncgr.org>
2018-03-12 10:48:42 -0600

    Fixed merge.

--- list-ontology-terms.sql
+++ list-ontology-terms.sql
@@ -1,19 +1,10 @@
 --
 -- list out the ontology terms, to find dupes
 --
-<<<<<<< HEAD
 SELECT ontologyid,id,identifier,substring(name,1,20) AS name,substring(description,1,20) AS description FROM ontologyterm 
 	WHERE identifier LIKE 'SO:%' ORDER BY identifier,name LIMIT 5;
 SELECT ontologyid,id,identifier,substring(name,1,20) AS name,substring(description,1,20) AS description FROM ontologyterm 
         WHERE identifier LIKE 'GO:%' ORDER BY identifier,name LIMIT 5;
-=======
-SELECT 'NULL' AS ontologyid,id,identifier,substring(name,1,20) AS name,substring(description,1,20) AS description FROM ontologyterm
-	WHERE ontologyid IS NULL ORDER BY identifier,name LIMIT 10;
-SELECT ontologyid,id,identifier,substring(name,1,20) AS name,substring(description,1,20) AS description FROM ontologyterm 
-        WHERE ontologyid=1000000 ORDER BY identifier,name LIMIT 10;
-SELECT ontologyid,id,identifier,substring(name,1,20) AS name,substring(description,1,20) AS description FROM ontologyterm
-        WHERE ontologyid=3000000 ORDER BY identifier,name LIMIT 10;
->>>>>>> 2756d480cde025ec8fa85eb6c054d72772ba9e2e
 SELECT ontologyid,id,identifier,substring(name,1,20) AS name,substring(description,1,20) AS description FROM ontologyterm 
         WHERE identifier LIKE 'PO:%' ORDER BY identifier,name LIMIT 5;
 SELECT ontologyid,id,identifier,substring(name,1,20) AS name,substring(description,1,20) AS description FROM ontologyterm 
