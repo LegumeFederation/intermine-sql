42b139ec02aa00e2cf476767530af3ddfac092e7
Sam Hokin <shokin@ncgr.org>
2018-03-20 08:16:50 -0600

    Tweak the cowpea variety descriptions.

---
+++ update-cowpea-descriptions.sql
@@ -0,0 +1,37 @@
+-- update the cowpea germplasm descriptions
+
+UPDATE organism SET description='TVu-14676 is a cowpea cultivar developed by IITA and is resistant to the parasitic plant Striga gesnerioides races SGl, SG2, SG3 and SG5.' WHERE variety='TVu-14676';
+
+UPDATE organism SET description='IT99K-573-1-1 is an early-maturing, white-seeded, high-yielding and Striga-resistant variety that was released in Nigeria under the name SAMPEA 14.' WHERE variety='IT99K-573-1-1';
+
+UPDATE organism SET description='Sanzi is a local landrace from Ghana which has a prostrate sprawling architecture, grayish-purple seeds, and a sub-globose leaf shape.' WHERE variety='Sanzi';
+
+UPDATE organism SET description='TVNu-1158 is small-seeded and has a perennial growth habit. It is cross-compatible with cultivated cowpea, although the F1 and subsequent generations show partial sterility.' WHERE variety='TVNu-1158';
+
+UPDATE organism SET description='Vita 7 is a spreading, indeterminate cowpea variety with hastate leaves and small seeds with smooth seed coats. It was developed by the Grain Legume Improvement Project (GLIP) of the International Institute of Tropical Agriculture (IITA) in Ibadan, Nigeria.' WHERE variety='Vita7';
+
+UPDATE organism SET description='California Blackeye 46 (8046) - Breeder: University of California, Davis, and the California Agric. Expt. Sta. Parentage: California Blackeye 5 x (California Blackeye 5 x PI 166146). Characteristics: more erect plant habit than California Blackeye 5; seeds are typical blackeye type, cream seed coat with black pigment around hilum; seed smaller than California Blackeye 5, 19-20 gm per 100 seed vs. 23 gm per 100 seed; 90 day maturity to dry seed; canning qualities comparable to California Blackeye 5 and California Blackeye 3; yields comparable to or better than those of California Blackeye 5. Resistance: Fusarium oxysporum f. sp. tracheiphilum race 3, homozygous for the Rk root knot nematode resistance gene. Crop Sci. 31:1703, 1991. 1987.' WHERE variety='CB46';
+
+UPDATE organism SET description='IT00K-1263 is high yielding and stays green under drought in Mozambique and Nigeria, with dark-brown seed.' WHERE variety='IT00K-1263';
+
+UPDATE organism SET description='Register of Australian grain legume cultivars: Vigna unguiculata L. (cowpea) cv. Big Buff: reg. no. ARGL 94-3, registered 14 December 1994.' WHERE variety='IT82E-18';
+
+UPDATE organism SET description='IT84S-2049 is high yielding under drought in Burkina Faso, with brown-eye seed and direct growth habit.' WHERE variety='IT84S-2049';
+
+UPDATE organism SET description='IT84S-2246-4 has combined resistance to aphid, bruchid, thrips and several diseases. It was developed by the Grain Legume Improvement Project (GLIP) of the International Institute of Tropical Agriculture (IITA) in Ibadan, Nigeria.' WHERE variety='IT84S-2246-4';
+
+UPDATE organism SET description='IT89KD-288 is high yielding under drought in Burkina Faso and Nigeria, with brown-eye seed. It is photoperiod sensitive.' WHERE variety='IT89KD-288';
+
+UPDATE organism SET description='Suvita-2, also known as Gorom, is a local cowpea landrace in Burkina Faso. It is resistant to the parasitic weed Striga and the fungal pathogen Macrophomina phaseolina.' WHERE variety='Suvita-2';
+
+UPDATE organism SET description='IT93K-503-1, a breeding line from the International Institute of Tropical Agriculture (IITA) breeding nursery in Nigeria, is draught tolerant and resistant to root-knot nematodes, M. phaseolina and Fusarium wilt.' WHERE variety='IT93K-503-1';
+
+UPDATE organism SET description='California Blackeye 27 (or CB27) is ideally suited to the Central Valley of California and other areas where hot weather occurs during flowering. This cultivar also produces higher blackeye bean yields in areas where races 3 and 4 of Fusarium wilt or aggressive biotypes of root-knot nematodes (like Meloidogyne incognita and M. Javanica) are present. Nematode populations in the soil are reduced following cultivation of CB27, conferring benefits to subsequent nematode susceptible crops. Therefore, CB27 is an excellent candidate for use in an annual crop rotation cycle in areas where control of root-knot nematodes is necessary.' WHERE variety='CB27';
+
+UPDATE organism SET description='Cowpea (<i>Vigna unguiculata</i>) is used in many parts of the world for its high-protein seeds, but also for its nutrient-rich edible leaves, forage, and soil enrichment. Cowpea is generally more heat-tolerant than common bean. Cowpeas are one of the most important food legume crops in the semi-arid tropics that cover Asia, Africa, southern Europe and Central and South America. A drought-tolerant and warm-weather crop, cowpeas are well-adapted to the drier regions of the tropics, where other food legumes do not perform as well. It also has the useful ability to fix atmospheric nitrogen through its root nodules, and it grows well in poor soils.' WHERE variety='IT97K-499-35';
+
+UPDATE organism SET description='IT97K-556-6 is an aphid-resistant cowpea line developed at IITA in Nigeria.' WHERE variety='IT97K-556-6';
+
+UPDATE organism SET description='ZN016 is a rustic landrace asparagus bean (<i>Vigna. unguiculata ssp. sesquipedialis</i>) variety originating from southern China.' WHERE variety='ZN016';
+
+UPDATE organism SET description='Zhijiang282 is a commercial asparagus bean(<i>Vigna. unguiculata ssp. sesquipedialis</i>) cultivar in China.' WHERE variety='Zhijiang282';
