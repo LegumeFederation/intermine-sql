d0398c70effe18f72ba0ee8335e236b48daf5368
Sam Hokin <shokin@ncgr.org>
2018-03-19 18:50:58 -0600

    Changed organism.comment to description.

--- list-geneticmaps.sql
+++ list-geneticmaps.sql
@@ -1,4 +1,9 @@
 --
 -- list out genetic maps to check for dupes
 --
-SELECT primaryidentifier,substring(description,1,50) AS description FROM geneticmap ORDER BY primaryidentifier;
+SELECT organismid,primaryidentifier,substring(description,1,50) AS description FROM geneticmap
+	ORDER BY organismid,primaryidentifier;
+
+SELECT primaryidentifier,pubmedid,doi,substring(title,1,50) AS title FROM geneticmap,publication,bioentitiespublications
+	WHERE geneticmap.id=bioentitiespublications.bioentities AND publication.id=bioentitiespublications.publications
+	ORDER BY primaryidentifier; 
--- list-to-annotations.sql
+++ list-to-annotations.sql
@@ -1,2 +1,5 @@
 -- list out QTL TO annotations to find dupes
-SELECT subjectid,ontologytermid,count(*) FROM ontologyannotation WHERE class LIKE '%TO%' GROUP BY subjectid,ontologytermid ORDER BY subjectid,ontologytermid; 
+-- SELECT subjectid,ontologytermid,count(*) FROM toannotation GROUP BY subjectid,ontologytermid ORDER BY subjectid,ontologytermid,count DESC;
+
+-- list out QTL TO annotations per organism
+SELECT count(*),commonname FROM toannotation,qtl,organism WHERE toannotation.subjectid=qtl.id AND qtl.organismid=organism.id GROUP BY commonname;  
