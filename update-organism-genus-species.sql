9d408685598067dff93da566e7db91085cfce76d
Sam Hokin <shokin@ncgr.org>
2018-04-24 12:18:21 -0600

    Updates for the new arahy load into PeanutMine (and elsewhere).

--- update-organism-genus-species.sql
+++ update-organism-genus-species.sql
@@ -6,7 +6,7 @@ UPDATE organism SET genus='Cicer', species='arietinum', name='Cicer arietinum (k
 UPDATE organism SET genus='Cicer', species='arietinum', name='Cicer arietinum (desi)', commonname='chickpea (desi)', shortname='C. arietinum (desi)' WHERE variety='ICC4958';
 UPDATE organism SET genus='Phaseolus', species='vulgaris', name='Phaseolus vulgaris', commonname='string bean', shortname='P. vulgaris' WHERE taxonid=3885;
 UPDATE organism SET genus='Glycine', species='max', name='Glycine max', commonname='soybean', shortname='G. max' WHERE taxonid=3847;
-UPDATE organism SET genus='Arachis', species='hypogaea', name='Arachis hypogaea', commonname='peanut', shortname='A. hypogaea ' WHERE taxonid=3818;
+UPDATE organism SET genus='Arachis', species='hypogaea', name='Arachis hypogaea', commonname='cultivated peanut', shortname='A. hypogaea ' WHERE taxonid=3818;
 UPDATE organism SET genus='Arachis', species='duranensis', name='Arachis duranensis', commonname='diploid duranensis peanut', shortname='A. duranensis' WHERE taxonid=130453;
 UPDATE organism SET genus='Arachis', species='ipaensis', name='Arachis ipaensis', commonname='dipload ipaensis peanut', shortname='A. ipaensis' WHERE taxonid=130454;
 UPDATE organism SET genus='Medicago', species='truncatula', name='Medicago truncatula', commonname='barrel medic', shortname='M. truncatula', variety='A17' WHERE taxonid=3880;
