0a5db80ddb9c5a4ba893dccc4535c23e246c3afd
Sam Hokin <shokin@ncgr.org>
2018-12-19 09:32:35 -0700

    Update strain descriptions.

--- update-strain-descriptions.sql
+++ update-strain-descriptions.sql
@@ -2,23 +2,23 @@
 -- General purpose strain description updater
 ---------------------------------------------
 
-UPDATE strain SET description=
+UPDATE strain SET name='Tifrunner', description=
 'Tifrunner (Reg. no. CV-93, PI 644011) is a runner-type peanut (Arachis hypogaea L. subsp. hypogaea var. hypogaea) cultivar that was released by the USDA-ARS and the Georgia Agricultural Experiment Stations in 2005. Tifrunner originated from a cross of F439-16-10-3 and PI 203396. Tifrunner is a runner market-type in seed and pod size. It has a spreading runner growth habit with an erect mainstem that is prominent throughout the growing season and at harvest. It has a high level of resistance to TSWV, and moderate resistance to early (Cercosporidium arachidicola Hori) and late leaf spot. It is a late maturity class peanut, with about 150 d needed for optimal maturity.'
 WHERE primaryidentifier='Tifrunner';
 
-UPDATE strain SET description=
+UPDATE strain SET name='TVu 14676', description=
 'TVu-14676 is a cowpea cultivar developed by IITA and is resistant to the parasitic plant Striga gesnerioides races SGl, SG2, SG3 and SG5.'
 WHERE primaryidentifier='TVu-14676';
 
-UPDATE strain SET description=
+UPDATE strain SET name='IT99K-573-1-1', description=
 'IT99K-573-1-1 is an early-maturing, white-seeded, high-yielding and Striga-resistant variety that was released in Nigeria under the name SAMPEA 14.'
 WHERE primaryidentifier='IT99K-573-1-1';
 
-UPDATE strain SET description=
+UPDATE strain SET name='Sanzi', description=
 'Sanzi is a local landrace from Ghana which has a prostrate sprawling architecture, grayish-purple seeds, and a sub-globose leaf shape.'
 WHERE primaryidentifier='Sanzi';
 
-UPDATE strain SET description=
+UPDATE strain SET name='TVNu 1158', description=
 'TVNu-1158 is small-seeded and has a perennial growth habit. It is cross-compatible with cultivated cowpea, although the F1 and subsequent generations show partial sterility.'
 WHERE primaryidentifier='TVNu-1158';
 
@@ -30,23 +30,23 @@ UPDATE strain SET name='California Blackeye 46', description=
 'California Blackeye 46 (8046) - Breeder: University of California, Davis, and the California Agric. Expt. Sta. Parentage: California Blackeye 5 x (California Blackeye 5 x PI 166146). Characteristics: more erect plant habit than California Blackeye 5; seeds are typical blackeye type, cream seed coat with black pigment around hilum; seed smaller than California Blackeye 5, 19-20 gm per 100 seed vs. 23 gm per 100 seed; 90 day maturity to dry seed; canning qualities comparable to California Blackeye 5 and California Blackeye 3; yields comparable to or better than those of California Blackeye 5. Resistance: Fusarium oxysporum f. sp. tracheiphilum race 3, homozygous for the Rk root knot nematode resistance gene. Crop Sci. 31:1703, 1991. 1987.'
 WHERE primaryidentifier='CB46';
 
-UPDATE strain SET description=
+UPDATE strain SET name='IT00K-1263', description=
 'IT00K-1263 is high yielding and stays green under drought in Mozambique and Nigeria, with dark-brown seed.'
 WHERE primaryidentifier='IT00K-1263';
 
-UPDATE strain SET description=
+UPDATE strain SET name='IT82E-18', description=
 'Register of Australian grain legume cultivars: Vigna unguiculata L. (cowpea) cv. Big Buff: reg. no. ARGL 94-3, registered 14 December 1994.'
 WHERE primaryidentifier='IT82E-18';
 
-UPDATE strain SET description=
+UPDATE strain SET name='IT84S-2049', description=
 'IT84S-2049 is high yielding under drought in Burkina Faso, with brown-eye seed and direct growth habit.'
 WHERE primaryidentifier='IT84S-2049';
 
-UPDATE strain SET description=
+UPDATE strain SET name='IT84S-2246-4', description=
 'IT84S-2246-4 has combined resistance to aphid, bruchid, thrips and several diseases. It was developed by the Grain Legume Improvement Project (GLIP) of the International Institute of Tropical Agriculture (IITA) in Ibadan, Nigeria.'
 WHERE primaryidentifier='IT84S-2246-4';
 
-UPDATE strain SET description=
+UPDATE strain SET name='IT89KD-288', description=
 'IT89KD-288 is high yielding under drought in Burkina Faso and Nigeria, with brown-eye seed. It is photoperiod sensitive.'
 WHERE primaryidentifier='IT89KD-288';
 
@@ -54,7 +54,7 @@ UPDATE strain SET name='Gorom', description=
 'Suvita-2, also known as Gorom, is a local cowpea landrace in Burkina Faso. It is resistant to the parasitic weed Striga and the fungal pathogen Macrophomina phaseolina.'
 WHERE primaryidentifier='Suvita-2';
 
-UPDATE strain SET description=
+UPDATE strain SET name='IT93K-503-1', description=
 'IT93K-503-1, a breeding line from the International Institute of Tropical Agriculture (IITA) breeding nursery in Nigeria, is draught tolerant and resistant to root-knot nematodes, M. phaseolina and Fusarium wilt.'
 WHERE primaryidentifier='IT93K-503-1';
 
@@ -62,23 +62,23 @@ UPDATE strain SET name='California Blackeye 27', description=
 'California Blackeye 27 (or CB27) is ideally suited to the Central Valley of California and other areas where hot weather occurs during flowering. This cultivar also produces higher blackeye bean yields in areas where races 3 and 4 of Fusarium wilt or aggressive biotypes of root-knot nematodes (like Meloidogyne incognita and M. Javanica) are present. Nematode populations in the soil are reduced following cultivation of CB27, conferring benefits to subsequent nematode susceptible crops. Therefore, CB27 is an excellent candidate for use in an annual crop rotation cycle in areas where control of root-knot nematodes is necessary.'
 WHERE primaryidentifier='CB27';
 
-UPDATE strain SET description=
+UPDATE strain SET name='IT97K-499-35', description=
 'Cowpea (Vigna unguiculata) is used in many parts of the world for its high-protein seeds, but also for its nutrient-rich edible leaves, forage, and soil enrichment. Cowpea is generally more heat-tolerant than common bean. Cowpeas are one of the most important food legume crops in the semi-arid tropics that cover Asia, Africa, southern Europe and Central and South America. A drought-tolerant and warm-weather crop, cowpeas are well-adapted to the drier regions of the tropics, where other food legumes do not perform as well. It also has the useful ability to fix atmospheric nitrogen through its root nodules, and it grows well in poor soils.'
 WHERE primaryidentifier='IT97K-499-35';
 
-UPDATE strain SET description=
+UPDATE strain SET name='IT97K-556-6', description=
 'IT97K-556-6 is an aphid-resistant cowpea line developed at IITA in Nigeria.'
 WHERE primaryidentifier='IT97K-556-6';
 
-UPDATE strain SET description=
+UPDATE strain SET name='ZN 016', description=
 'ZN016 is a rustic landrace asparagus bean (Vigna. unguiculata ssp. sesquipedialis) variety originating from southern China showing resistance to powdery mildew at all growth stages.'
 WHERE primaryidentifier='ZN016';
 
-UPDATE strain SET description=
+UPDATE strain SET name='Zhijiang 282', description=
 'Zhijiang282 is a commercial asparagus bean (Vigna. unguiculata ssp. sesquipedialis) cultivar in China which is highly susceptible to powdery mildew.'
 WHERE primaryidentifier='Zhijiang282';
 
-UPDATE strain SET description=
+UPDATE strain SET name='G19833', description=
 'The Andean common bean landrace G19833 (Chaucha Chuga) was used to obtain the first reference genome of Phaseolus vulgaris. G19833 has been reported as resistant to the anthracnose, angular leaf spot, and Ascochyta blight pathogens and susceptible to the bean golden mosaic and bean common mosaic viruses.'
 WHERE primaryidentifier='G19833';
 
@@ -90,11 +90,11 @@ UPDATE strain SET name='ICPL 87119', description=
 'ICPL 87119 is a medium-duration variety with an indeterminate growth habit. Its stem is green and its flowers are yellow, with red streaks on the back of the standard petal. The pods are green with maroon streaks. Its plant height ranges from 140 to 227 cm. Its time to 50% flowering ranges between 110 and 125 days and it takes 160 to 202 days for 75% maturity. The seeds of ICPL 87119 are brown and oval with a 100-seed mass ranging from 10.2 to 11.2 g.'
 WHERE primaryidentifier='ICPL87119';
 
-UPDATE strain SET description=
+UPDATE strain SET name='K30076', description=
 'A. ipaensis K30076 was collected by A. Krapovickas, W.C. Gregory, D.J. Banks, J.R. Pietrarelli, A. Schinini and C.E. Simpson in 1977, and is the only available accession in germplasm collections worldwide. It originated from the same collection site as the holotype of this species, the species'' only known site of occurrence, ~30 km north of Villa Montes, Bolivia.'
 WHERE primaryidentifier='K30076';
 
-UPDATE strain SET description=
+UPDATE strain SET name='V14167', description=
 'A. duranensis V14167, a yellow-flowered accession, was collected by J.F.M. Valls, L. Novara and A. Etcheverryin in 1997 from Ruta Nacional 51, Estacinnn Alvarado, near Tabacalera, Argentina, at an elevation of 1,206 m.'
 WHERE primaryidentifier='V14167';
 
